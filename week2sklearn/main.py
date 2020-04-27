59ec91da43a34cecce5aa4929187ad2628bf9558
shounak <shounak2922k@gmail.com>
2020-04-27 12:46:37 +0530

    changing sklearn implementation files

--- week2sklearn/main.py
+++ week2sklearn/main.py
@@ -9,29 +9,20 @@ X = dataset.iloc[:, 0].values                                        # read firs
 y = dataset.iloc[:, 1].values
 
 
-Xtr,Xte,Ytr,Yte=train_test_split(X, y, test_size=0.25, random_state=0)
+Xtr,Xte,Ytr,Yte=train_test_split(X, y, test_size=0.2, random_state=0)
 
 regressor=LinearRegression()
-Xtr1=Xtr.reshape(1, -1)
-Ytr1=Ytr.reshape(1, -1)
+Xtr1=Xtr.reshape(-1, 1)
+Ytr1=Ytr.reshape(-1, 1)
 regressor.fit(Xtr1,Ytr1)
 plt.scatter(Xtr1, Ytr1)
 plt.title('X versus Y')
 plt.show()
-plt.scatter(Xtr, Ytr)
-plt.title('X versus Y')
-plt.show()
-plt.scatter(X, y)
-plt.title('X versus Y')
-plt.show()
-
-
-
-Xte1=Xte.reshape(1, -1)
-prediction=regressor.predict(Xte.reshape(-1,1))
+Xte1=Xte.reshape(-1,1)
+prediction=regressor.predict(Xte1)
 print(prediction)
 plt.scatter(Xtr, Ytr)
 
-plt.plot(Xtr,  regressor.predict(Xtr), color='red')
+plt.plot(Xtr1,  regressor.predict(Xtr1), color='red')
 plt.title('X versus Y')
 plt.show()
